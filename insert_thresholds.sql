cb8e4925f09a67865c3b7f6132eae4570ff9500e
Morgan Rozman <morganrozman@Morgans-MBP-2.attlocal.net>
2020-04-21 15:09:55 -0500

    Upload insert_thresholds.sql to add thresholds to db.

---
+++ insert_thresholds.sql
@@ -0,0 +1,3 @@
+INSERT INTO Threshold (strain_id, media_id, run_date, OD24hr, OD48hr, OD72hr)
+VALUES ('7', '1', '2017-01-01', '0.160', '0.116', '0.110')
+('7', '2', '2017-01-01', '0.251', '0.252', '0.280');
